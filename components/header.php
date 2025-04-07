953a87940b70f5be23728029c916d58abe10fc4b
valentin <valentin.granata@gmail.com>
2025-04-07 13:48:40 +0200

    add post first interface

--- components/header.php
+++ components/header.php
@@ -1,3 +1,34 @@
-<header>
+<header class="column glass">
+    <a class="profile row">
+        <img class="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShYwM33sSN7MtnLIq0k1qjhpoEtSstLE26gA&s" alt="avatar">
+        <h1 class="username">Granatino</h1>
+    </a>
+
+    <section class="column">
+        <nav class="column">
+            <a class="btn row">
+                <img src="./assets/img/home.png" alt="icon">
+                <p>Home</p>
+            </a>
+            <a class="btn row">
+                <img src="./assets/img/search.png" alt="icon">
+                <p>Search</p>
+            </a>
+            <a class="btn row">
+                <img src="./assets/img/profile.png" alt="icon">
+                <p>Profile</p>
+            </a>
+            <a class="btn row">
+                <img src="./assets/img/settings.png" alt="icon">
+                <p>Settings</p>
+            </a>
+        </nav>
+
+        <a href="#" class="post row glass">Post</a>
+    </section>
+
+    <footer class="row">
+        <p>All rights reserved</p>
+    </footer>
     
 </header>
--- index.php
+++ index.php
@@ -9,11 +9,71 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Blog - GZZ</title>
-    </head>
-    <body>
-
         
+        <title>Blog - GZZ</title>
+
+        <link rel="stylesheet" href="./assets/style/style.css">
+    </head>
+    <body class="row">
+
+        <?php include_once "./components/header.php"; ?>
+
+        <section class="main glass column">
+            <div class="post-control">
+                <div class="control glass">
+                    <img src="./assets/img/up.png" alt="">
+                </div>
+
+                <div class="control glass">
+                    <img src="./assets/img/down.png" alt="">
+                </div>
+            </div>
+
+            <div class="user row">
+                <img src="./assets/img/profile.png" alt="" class="avatar">
+                <h1 class="username">Tremoner</h1>
+            </div>
+
+            <div class="content column glass">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque fugit nesciunt iure iusto fuga nostrum obcaecati earum assumenda dignissimos explicabo, delectus distinctio eius. Obcaecati, delectus quos repellendus odio velit blanditiis!</p>
+                
+                <div class="photo row">
+                    <img class="glass" src="https://www.shutterstock.com/image-vector/img-vector-icon-design-on-260nw-2164648583.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="interaction row">
+                <div class="left">
+                    <p class="date">07/04/2025</p>
+                </div>
+
+                <div class="right row">
+                    <div class="info share">
+                        <a href="">
+                            <img src="./assets/img/share.png" alt="">
+                        </a>
+                    </div>
+
+                    <div class="info repost row">
+                        <a href="">
+                            <img src="./assets/img/repost.png" alt="">
+                        </a>
+                        <p>123</p>
+                    </div>
+
+                    <div class="info like row">
+                        <a href="">
+                            <img src="./assets/img/like.png" alt="">
+                        </a>
+                        <p>234</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="about glass">
+
+        </section>
         
     </body>
 </html>
